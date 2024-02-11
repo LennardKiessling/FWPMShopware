57d877efd6bcd33293d195bef59ca4562a524df6
Lennard Kießling <lennard-2@web.de>
2024-02-11 22:34:10 +0100

    added compability for all the navigation settings to work seperatly

--- app/storefront/src/main.js
+++ app/storefront/src/main.js
@@ -11,22 +11,39 @@ if (navigationOnScroll){
     // Aktuelle Scroll-Position
     let aktuelleScrollPosition = scrollY;
 
-    if (aktuelleScrollPosition > letzteScrollPosition) {
-        // Nach unten gescrollt
-        if (Header.classList.contains('header-hidden')){
-            //do nothing
-        } else {
-            Header.classList.add('header-hidden');
-            Navigation.classList.add('header-hidden');
-            ContentMain.classList.add('header-hidden');
-        }
+        if (stickyNavigation){
+            if (aktuelleScrollPosition > letzteScrollPosition) {
+                // Nach unten gescrollt
+                if (Header.classList.contains('header-hidden')){
+                    //do nothing
+                } else {
+                    Header.classList.add('header-hidden');
+                    Navigation.classList.add('header-hidden');
+                    ContentMain.classList.add('header-hidden');
+                }
 
-        } else if (aktuelleScrollPosition < letzteScrollPosition) {
-        // Nach oben gescrollt
-            if (Header.classList.contains('header-hidden')){
-                Header.classList.remove('header-hidden');
-                Navigation.classList.remove('header-hidden');
-                ContentMain.classList.remove('header-hidden');
+            } 
+            else if (aktuelleScrollPosition < letzteScrollPosition) {
+                // Nach oben gescrollt
+                    if (Header.classList.contains('header-hidden')){
+                        Header.classList.remove('header-hidden');
+                        Navigation.classList.remove('header-hidden');
+                        ContentMain.classList.remove('header-hidden');
+                    }
+            }
+        } else {
+            if (aktuelleScrollPosition > letzteScrollPosition) {
+                // Nach unten gescrollt
+                if (Header.classList.contains('sticky-header')){
+                    Header.classList.remove('sticky-header');
+                    Navigation.classList.remove('sticky-header');
+                    ContentMain.classList.remove('sticky-header');
+                }
+            } 
+            else if (aktuelleScrollPosition < letzteScrollPosition) {
+                    Header.classList.add('sticky-header');
+                    Navigation.classList.add('sticky-header');
+                    ContentMain.classList.add('sticky-header');
             }
         }
 
