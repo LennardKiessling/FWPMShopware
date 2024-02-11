22dd8b800ff713ecf20348e78ef27ef0aa261f17
Lennard Kießling <lennard-2@web.de>
2024-02-11 18:11:00 +0100

    Javascript Added for Navigation Scroll Functionality

--- app/storefront/src/main.js
+++ app/storefront/src/main.js
@@ -1,1 +1,34 @@
-import 'js/navigation-scroll.js';
+Header = document.querySelector('.header-main');
+Navigation = document.querySelector('.nav-main');
+ContentMain = document.querySelector('.content-main');
+
+// Variable zur Speicherung der letzten Scroll-Position
+let letzteScrollPosition = scrollY;
+
+// Event Listener für das Scroll-Event
+window.addEventListener('scroll', function() {
+  // Aktuelle Scroll-Position
+  let aktuelleScrollPosition = scrollY;
+
+  if (aktuelleScrollPosition > letzteScrollPosition) {
+    // Nach unten gescrollt
+    if (Header.classList.contains('header-hidden')){
+        //do nothing
+    } else {
+        Header.classList.add('header-hidden');
+        Navigation.classList.add('header-hidden');
+        ContentMain.classList.add('header-hidden');
+    }
+
+    } else if (aktuelleScrollPosition < letzteScrollPosition) {
+    // Nach oben gescrollt
+        if (Header.classList.contains('header-hidden')){
+            Header.classList.remove('header-hidden');
+            Navigation.classList.remove('header-hidden');
+            ContentMain.classList.remove('header-hidden');
+        }
+    }
+
+  // Aktualisierung der letzten Scroll-Position für den nächsten Event
+  letzteScrollPosition = aktuelleScrollPosition;
+});
