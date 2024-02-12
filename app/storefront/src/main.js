4c90ed20efcb5cada991d4f89c94b1f34b58ee84
Lennard Kießling <lennard-2@web.de>
2024-02-12 11:51:51 +0100

    Javascript Optimiert, letzte Fehler behoben und Performance vom Script erhöht

--- app/storefront/src/main.js
+++ app/storefront/src/main.js
@@ -1,7 +1,6 @@
 if (navigationOnScroll){
     Header = document.querySelector('.header-main');
     Navigation = document.querySelector('.nav-main');
-    ContentMain = document.querySelector('.content-main');
 
     // Variable zur Speicherung der letzten Scroll-Position
     let letzteScrollPosition = scrollY;
@@ -19,7 +18,6 @@ if (navigationOnScroll){
                 } else {
                     Header.classList.add('header-hidden');
                     Navigation.classList.add('header-hidden');
-                    ContentMain.classList.add('header-hidden');
                 }
 
             } 
@@ -28,7 +26,6 @@ if (navigationOnScroll){
                     if (Header.classList.contains('header-hidden')){
                         Header.classList.remove('header-hidden');
                         Navigation.classList.remove('header-hidden');
-                        ContentMain.classList.remove('header-hidden');
                     }
             }
         } else {
@@ -37,13 +34,11 @@ if (navigationOnScroll){
                 if (Header.classList.contains('sticky-header')){
                     Header.classList.remove('sticky-header');
                     Navigation.classList.remove('sticky-header');
-                    ContentMain.classList.remove('sticky-header');
                 }
             } 
             else if (aktuelleScrollPosition < letzteScrollPosition) {
                     Header.classList.add('sticky-header');
                     Navigation.classList.add('sticky-header');
-                    ContentMain.classList.add('sticky-header');
             }
         }
 
